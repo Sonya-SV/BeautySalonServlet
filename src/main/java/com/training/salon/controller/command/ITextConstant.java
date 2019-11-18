bc6a21b482ded3f8340023a02bcb678eb70f5b5d
Sonya <sofi.svyrydenko@gmail.com>
2019-11-18 15:40:05 +0200

    add interface for string constants

---
+++ src/main/java/com/training/salon/controller/command/ITextConstant.java
@@ -0,0 +1,14 @@
+package com.training.salon.controller.command;
+
+public interface ITextConstant {
+
+    String USER_ERROR = "Invalid login or password";
+    String NOT_UNIQUE_EMAIL = "Not unique email";
+    String ALREADY_BOOKED = "Already booked";
+    String SUCCESS_SEND = "Thank you for your comment";
+    String EMPTY_COMMENT = "Comment is empty";
+    String PROCEDURE_ERROR = "No such procedure";
+    String DISCREPANCY = "Master doesn't perform such procedure";
+    String PASSWORD_DIFFERENT = "Password are different";
+    String SUCCESS_SAVE = "Saved successfully";
+}
