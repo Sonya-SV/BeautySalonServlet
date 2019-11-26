9506286c980fd75a3a93b45e2e79492a9e57e879
Sonya <sofi.svyrydenko@gmail.com>
2019-11-26 21:34:49 +0200

    code refactor

--- src/main/java/com/training/salon/model/dao/ScheduleDao.java
+++ src/main/java/com/training/salon/model/dao/ScheduleDao.java
@@ -3,8 +3,6 @@ package com.training.salon.model.dao;
 import com.training.salon.controller.exception.BookException;
 import com.training.salon.model.entity.Schedule;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 
 public interface ScheduleDao extends GenericDao<Schedule> {
--- src/main/java/com/training/salon/model/service/ScheduleService.java
+++ src/main/java/com/training/salon/model/service/ScheduleService.java
@@ -2,14 +2,9 @@ package com.training.salon.model.service;
 
 import com.training.salon.controller.exception.BookException;
 import com.training.salon.model.dao.DaoFactory;
-import com.training.salon.model.dao.ProcedureDao;
 import com.training.salon.model.dao.ScheduleDao;
-import com.training.salon.model.entity.Procedure;
 import com.training.salon.model.entity.Schedule;
 
-import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
