652137d159694c46152698cb0465560bc9f247c7
Sonya <sofi.svyrydenko@gmail.com>
2019-11-26 21:49:19 +0200

    find comment for master

--- src/main/java/com/training/salon/model/dao/CommentDao.java
+++ src/main/java/com/training/salon/model/dao/CommentDao.java
@@ -2,6 +2,9 @@ package com.training.salon.model.dao;
 
 import com.training.salon.model.entity.Comment;
 
+import java.util.List;
+
 public interface CommentDao extends GenericDao<Comment> {
     void createComment(String comment, Long masterId, Long userId);
+    List<Comment> findAllByMaster(Long masterId);
 }
