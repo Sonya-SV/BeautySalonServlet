807f0ab9b780374bf95a6bf7ada023b70543d220
Sonya <sofi.svyrydenko@gmail.com>
2019-11-27 01:59:53 +0200

    add exception for creating schedule note

--- src/main/java/com/training/salon/controller/command/ScheduleNote.java
+++ src/main/java/com/training/salon/controller/command/ScheduleNote.java
@@ -2,14 +2,17 @@ package com.training.salon.controller.command;
 
 import com.training.salon.controller.exception.BookException;
 import com.training.salon.model.entity.Schedule;
-import com.training.salon.model.entity.User;
 import com.training.salon.model.service.ScheduleService;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
-import java.time.LocalDate;
-import java.time.LocalTime;
+
+import static com.training.salon.controller.command.ITextConstant.ALREADY_BOOKED;
+import static com.training.salon.controller.command.ITextConstant.UNABLE_TO_CREATE_RECORD;
 
 public class ScheduleNote implements ICommand {
+    private  static final Logger log = LogManager.getLogger(Registration.class);
     private ScheduleService scheduleService;
 
     public ScheduleNote(ScheduleService scheduleService) {
@@ -20,27 +23,21 @@ public class ScheduleNote implements ICommand {
     public String execute(HttpServletRequest request) {
         String firstName = request.getParameter("firstName");
         String lastName = request.getParameter("lastName");
-        LocalDate date = LocalDate.parse(request.getParameter("date"));
-        LocalTime time = LocalTime.parse(request.getParameter("time"));
-
 
         Schedule schedule = (Schedule) request.getSession().getAttribute("schedule");
         schedule.setClientFirstName(firstName);
         schedule.setClientLastName(lastName);
-        schedule.setDate(date);
-        schedule.setTime(time);
         schedule.setDone(false);
-//        request.setAttribute("schedule", schedule);
-//        request.setAttribute("user", user);
         try {
             scheduleService.saveToSchedule(schedule);
-            System.out.println("saved");
-//            return "redirect:/user/booking";
+            log.info("Note added to the master (" +schedule.getMaster().getUser().getFirstName() + ") Schedule");
         } catch (BookException e) {
-            System.out.println("already booked");
-            request.setAttribute("alreadyBooked", "Already booked");
-            return "/WEB-INF/user/order.jsp";
+            request.setAttribute("alreadyBooked", ALREADY_BOOKED);
+            return "/WEB-INF/"+request.getSession().getAttribute("role").toString().toLowerCase()+"/order.jsp";
+        } catch (java.lang.Exception e){
+            request.setAttribute("errorOrder", UNABLE_TO_CREATE_RECORD);
+            return "/WEB-INF/"+request.getSession().getAttribute("role").toString().toLowerCase()+"/order.jsp";
         }
-        return "/WEB-INF/user/successpage.jsp";
+        return "redirect:/successpage";
     }
 }
