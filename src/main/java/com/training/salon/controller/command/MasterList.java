7519e117896c5123959532bd7ff556ad231e43cb
Sonya <sofi.svyrydenko@gmail.com>
2019-11-27 01:57:02 +0200

    refactor pagination

--- src/main/java/com/training/salon/controller/command/MasterList.java
+++ src/main/java/com/training/salon/controller/command/MasterList.java
@@ -1,13 +1,12 @@
 package com.training.salon.controller.command;
 
 import com.training.salon.model.entity.Master;
-import com.training.salon.model.entity.User;
 import com.training.salon.model.service.MasterService;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
-public class MasterList implements ICommand{
+public class MasterList implements ICommand {
 
     private MasterService masterService;
 
@@ -18,24 +17,17 @@ public class MasterList implements ICommand{
     @Override
     public String execute(HttpServletRequest request) {
 
-        List<Master> masters = masterService.getAllMasters();
-        int page=1;
-        int rowsPerPage=8;
+        int page = 1;
+        int rowsPerPage = 8;
         if (request.getParameter("page") != null) page = Integer.parseInt(request.getParameter("page"));
-        int noOfPages = (int) (Math.ceil((double)masters.size() / rowsPerPage));
+        List<Master> masters = masterService.getAllMasters();
+        int noOfPages = (int) (Math.ceil((double) masters.size() / rowsPerPage));
 
-        request.setAttribute("masters",
-                masters.subList((page-1)*rowsPerPage,
-                        Math.min((page-1)*rowsPerPage+ rowsPerPage,
-                                masters.size())));
-
+        request.setAttribute("masters", masters.subList((page - 1) * rowsPerPage,
+                Math.min((page - 1) * rowsPerPage + rowsPerPage, masters.size())));
         request.setAttribute("noOfPages", noOfPages);
         request.setAttribute("currentPage", page);
-
-        if(request.getSession().getAttribute("role").equals(User.Role.ADMIN))
-            return "/WEB-INF/admin/masterlist.jsp";
-        else if(request.getSession().getAttribute("role").equals(User.Role.MASTER))
-            return "/WEB-INF/master/masterlist.jsp";
-        return "/WEB-INF/user/masterlist.jsp";
+        return "/WEB-INF/" + request.getSession().getAttribute("role").toString().toLowerCase() + "/masterlist.jsp";
     }
 }
+
