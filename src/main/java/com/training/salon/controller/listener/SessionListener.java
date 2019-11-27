71eb889e1905334327ae17663709a519c639b0a5
Sonya <sofi.svyrydenko@gmail.com>
2019-11-27 03:09:39 +0200

    fix clearing loggedUsers

--- src/main/java/com/training/salon/controller/listener/SessionListener.java
+++ src/main/java/com/training/salon/controller/listener/SessionListener.java
@@ -1,11 +1,14 @@
 package com.training.salon.controller.listener;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 import java.util.HashSet;
 
 public class SessionListener implements HttpSessionListener {
-//    private static final Logger logger = LogManager.getLogger(SessionListener.class);
+    private static final Logger log = LogManager.getLogger(SessionListener.class);
 
     @Override
     public void sessionCreated(HttpSessionEvent httpSessionEvent) {
@@ -14,14 +17,14 @@ public class SessionListener implements HttpSessionListener {
 
     @Override
     public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
-//        @SuppressWarnings("unchecked")
+        @SuppressWarnings("unchecked")
         HashSet<String> loggedUsers = (HashSet<String>) httpSessionEvent
                 .getSession().getServletContext()
                 .getAttribute("loggedUsers");
-        String email = (String) httpSessionEvent.getSession()
-                .getAttribute("userEmail");
+        String email = (String) httpSessionEvent.getSession().getServletContext()
+                .getAttribute("email");
+        log.info("User log out: " + email);
         loggedUsers.remove(email);
-//        logger.info("User log out: " + email);
         httpSessionEvent.getSession().getServletContext().setAttribute("loggedUsers", loggedUsers);
     }
 }
