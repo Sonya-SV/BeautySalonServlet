803580f5fe4bdfa34f3e68e901fc89a401defd62
Sonya <sofi.svyrydenko@gmail.com>
2019-12-09 11:16:47 +0200

    change db

--- src/main/java/com/training/salon/model/dao/impl/ConnectionPoolHolder.java
+++ src/main/java/com/training/salon/model/dao/impl/ConnectionPoolHolder.java
@@ -13,7 +13,7 @@ public class ConnectionPoolHolder {
                 if (dataSource == null) {
                     BasicDataSource ds = new BasicDataSource();
                     ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
-                    ds.setUrl("jdbc:mysql://localhost:3306/beauty_salon_db?serverTimezone=UTC");
+                    ds.setUrl("jdbc:mysql://localhost:3306/beauty_salon?serverTimezone=UTC");
                     ds.setUsername("root");
                     ds.setPassword("root");
                     ds.setMinIdle(5);
